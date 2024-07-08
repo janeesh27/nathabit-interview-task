2c28ed5994b28551541ea1b258c2ebb4f19ee102
janeesh27 <janeesh.tin@gmail.com>
2024-07-08 15:08:45 +0530

    Margin Changed

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -7,7 +7,7 @@ const MovieList = ({ movies }) => {
       {movies.map((movie) => (
         <div key={movie.imdbID} className="movie-item p-5 bg-black">
           <img src={movie.Poster} alt={movie.Title} />
-          <div className="text-white flex flex-col mt-2 gap-y-2">
+          <div className="text-white flex flex-col mt-4 gap-y-2">
             <h3>{movie.Title}</h3>
             <p>{movie.Year}</p>
           </div>
